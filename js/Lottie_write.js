66445b58413a6e52a4df4d5ea5ca6a66cfe49d68
Tezuka-S <93756640+Tezuka-S@users.noreply.github.com>
2021-11-05 15:46:14 +0900

    Add files via upload

---
+++ js/Lottie_write.js
@@ -0,0 +1,15 @@
+lottie.loadAnimation({
+  container: document.getElementById('lottie'), // アニメーションをさせたいdivなどhtml要素ににつけた名前
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'json/mail_test3.json' // 自分が書き出したjsonファイルの名前と場所（パス）を記述
+});
+
+lottie.loadAnimation({
+  container: document.getElementById('Load'), // アニメーションをさせたいdivなどhtml要素ににつけた名前
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'json/Load.json' // 自分が書き出したjsonファイルの名前と場所（パス）を記述
+});
